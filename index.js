2e61f4552c7050f5e2605f0f65073296fc7c77ce
Leandro <leandrolopez131982@gmail.com>
2023-10-27 12:48:15 -0300

    package.json

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const rootElement = document.getElementById("root");
-
-const newDiv = document.createElement("div");
-
-const newH1 = document.createElement("h1");
-newH1.textContent = "Hello world";
-
-newDiv.appendChild(newH1);
-
-rootElement.appendChild(newDiv);
+console.log("Hola mundo")
